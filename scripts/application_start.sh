5ed22e485e6d792999f3943c45314f6a711c4383
Balasuriyabala <111849019+Balasuriyabala@users.noreply.github.com>
2023-11-23 12:27:57 +0530

    Delete scripts directory

--- scripts/application_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/repo name/deploy.log
-
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ec2-user/repo name/deploy.log
-cd /home/ec2-user/repo name >> /home/ec2-user/repo name/deploy.log
-
-echo 'npm install' >> /home/ec2-user/repo name/deploy.log 
-npm install >> /home/ec2-user/repo name/deploy.log
